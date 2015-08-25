7df3f11ae9e3203e8c36707ebf9c07cd52aa5aa3
pramod <pramodpal68@gmail.com>
2015-08-25 10:59:42 +0530

    ok

--- app/src/main/java/com/org/koizen12/mysampleproject/SampleActivity.java
+++ app/src/main/java/com/org/koizen12/mysampleproject/SampleActivity.java
@@ -11,6 +11,7 @@ public class SampleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample);
+        // this is sample class.
     }
 
     @Override
